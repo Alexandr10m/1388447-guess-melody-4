35da425b178b5fd2e3642dd3d6b376433f3e07e9
Alexandr10m <serbackij@gmail.com>
2020-06-27 16:16:43 +0300

    Инициализировал Store. Добавил reducer с тестами

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import App from "./components/app/app.jsx";
 import questions from "./mocks/questions.js";
+import {reducer} from "./reducer.js";
 
 const SETTINGS = {
   errorsCount: 3,
 };
 
+const reduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f;
+
+const store = createStore(reducer, reduxDevTools);
+
 ReactDOM.render(
-    <App
-      errorsCount={SETTINGS.errorsCount}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={SETTINGS.errorsCount}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
