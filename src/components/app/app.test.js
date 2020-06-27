ff5fac2d80132984d0cbceb89e331bdc048fd9f6
Alexandr10m <serbackij@gmail.com>
2020-06-27 19:09:22 +0300

    Связал компонент App со Store. Обновил тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import App from "./app.jsx";
+import {App} from "./app.jsx";
 
 
 const questions = [
@@ -39,14 +39,52 @@ const questions = [
   },
 ];
 
-describe(`Snapshot of App `, () => {
-  it(`AppComponent should render`, () => {
+
+describe(`Snapshot of App`, () => {
+  it(`Render WelcomeScreen`, () => {
     const tree = renderer
-      .create(
-          <App
-            errorsCount={3}
-            questions={questions}
-          />)
+    .create(<App
+      errorsCount={3}
+      questions={questions}
+      onUserAnswer={() => {}}
+      onWelcomeButtonClick={() => {}}
+      step={-1}
+    />)
+    .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`Render GenreQuestionScreen`, () => {
+    const tree = renderer
+      .create(<App
+        errorsCount={3}
+        questions={questions}
+        onUserAnswer={() => {}}
+        onWelcomeButtonClick={() => {}}
+        step={0}
+      />, {
+        createNodeMock: () => {
+          return {};
+        }
+      })
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`Render ArtistQuestionScreen`, () => {
+    const tree = renderer
+      .create(<App
+        errorsCount={3}
+        questions={questions}
+        onUserAnswer={() => {}}
+        onWelcomeButtonClick={() => {}}
+        step={1}
+      />, {
+        createNodeMock: () => {
+          return {};
+        }
+      })
       .toJSON();
 
     expect(tree).toMatchSnapshot();
