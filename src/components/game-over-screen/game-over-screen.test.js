bb6b92442ce6496452479be19e669c8242f463ef
Alexandr10m <serbackij@gmail.com>
2020-07-04 20:11:54 +0300

    Добавил тесты для новых компонентов

---
+++ src/components/game-over-screen/game-over-screen.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import GameOverScreen from "./game-over-screen.jsx";
+
+describe(`Snapshot of GameOverScreen`, () =>{
+  it(`Should GameOverScreen render correctly`, () => {
+    const tree = renderer
+      .create(
+          <GameOverScreen
+            onReplayButtonClick={() => {}}
+          />)
+          .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
