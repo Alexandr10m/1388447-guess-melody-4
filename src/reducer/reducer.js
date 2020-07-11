4e9c3a5d121982c68206b1a67a6ddfa41baec4c5
Alexandr10m <serbackij@gmail.com>
2020-07-11 09:37:41 +0300

    Добавил новый reducer - user

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -1,6 +1,7 @@
 import {combineReducers} from "redux";
 import {reducer as data} from "./data/data.js";
 import {reducer as game} from "./game/game.js";
+import {reducer as user} from "./user/user.js";
 
 import NameSpace from "./name-space.js";
 
@@ -8,4 +9,5 @@ import NameSpace from "./name-space.js";
 export default combineReducers({
   [NameSpace.DATA]: data,
   [NameSpace.GAME]: game,
+  [NameSpace.USER]: user,
 });
